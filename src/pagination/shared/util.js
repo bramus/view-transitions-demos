5d3f1eca3a3fe719a56ecf6ac1ebf0ac0287a197
Bramus <bramus@bram.us>
2024-03-05 00:06:33 +0100

    ✨ Pagination: SPA: Add classic SPA demo, promoting the previous one to SPA+Types

---
+++ src/pagination/shared/util.js
@@ -0,0 +1,108 @@
+const clamp = (val, min, max) => Math.min(Math.max(val, min), max);
+
+const createElement = (tag, attributes = {}) => {
+	const $el = document.createElement(tag);
+	for (const [attr, attrValue] of Object.entries(attributes)) {
+		switch (attr) {
+			case 'class':
+				attrValue.split(' ').forEach(className => {
+					$el.classList.add(className);
+				});
+				break;
+			case 'children':
+				attrValue.forEach($child => {
+					$el.appendChild($child);
+				});
+				break;
+			case 'innerText':
+				$el.innerText = attrValue;
+				break;
+			case 'innerHTML':
+				$el.innerHTML = attrValue;
+				break;
+			default:
+				if ([true, false].includes(attrValue)) {
+					if (attrValue) {
+						$el.setAttribute(attr, '');
+					}
+				} else {
+					$el.setAttribute(attr, attrValue);
+				}
+		}
+	}
+	return $el;
+};
+
+// Function to generate the markup needed for the pagination
+const generatePagination = (currentPageIndex, numPages, pathName = 'index.html') => {
+	const $ul = createElement('ul', {
+		class: 'pagination',
+	});
+
+	// First Page
+	if (currentPageIndex == 1) {
+		$ul.appendChild(createElement('li', {
+			'data-pagination-disabled': true,
+			'data-pagination-prev': true,
+			children: [
+				createElement('span', {
+					innerHTML: '&lsaquo;',
+				})
+			]
+		}));
+	} else {
+		$ul.appendChild(createElement('li', {
+			'data-pagination-prev': true,
+			children: [
+				createElement('a', {
+					href: `${pathName}?p=${currentPageIndex - 1}`,
+					title: 'Go to Previous Page',
+					innerHTML: '&lsaquo;',
+				})
+			]
+		}));
+	}
+
+	// Pages themselves
+	for (let p = 1; p <= numPages; p++) {
+		$ul.appendChild(createElement('li', {
+			'data-pagination-current': p == currentPageIndex,
+			children: [
+				createElement('a', {
+					href: `${pathName}?p=${p}`,
+					title: `Go to Page ${p}`,
+					innerHTML: p,
+				})
+			]
+		}));
+
+	}
+
+	// Last Page
+	if (currentPageIndex == numPages) {
+		$ul.appendChild(createElement('li', {
+			'data-pagination-disabled': true,
+			'data-pagination-next': true,
+			children: [
+				createElement('span', {
+					innerHTML: '&rsaquo;',
+				})
+			]
+		}));
+	} else {
+		$ul.appendChild(createElement('li', {
+			'data-pagination-next': true,
+			children: [
+				createElement('a', {
+					href: `${pathName}?p=${currentPageIndex + 1}`,
+					title: 'Go to Next Page',
+					innerHTML: '&rsaquo;',
+				})
+			]
+		}));
+	}
+
+	return $ul;
+}
+
+export { clamp, createElement, generatePagination };
