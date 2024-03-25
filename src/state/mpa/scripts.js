6f4540c5b17e75fb5cee46c3bb4204a0b62ac36d
Bramus <bramus@bram.us>
2024-03-26 00:22:29 +0100

    ♻️ MPA State Transfer: Refactor to read/write more state

--- src/state/mpa/scripts.js
+++ src/state/mpa/scripts.js
@@ -1,17 +1,38 @@
+const getVideoState = ($video) => {
+	return {
+		currentTime: $video.currentTime,
+		paused: $video.paused,
+		muted: $video.muted,
+	};
+};
+
+const restoreVideoState = ($video, state) => {
+	$video.currentTime = state.currentTime;
+	$video.muted = state.muted;
+	if (state.paused === false) $video.play();
+};
+
 window.addEventListener('pageswap', async (e) => {
 	if (e.viewTransition) {
-		sessionStorage.setItem("currentTime", document.querySelector('video').currentTime);
+		const $video = document.querySelector('video');
+		const videoState = getVideoState($video);
+		sessionStorage.setItem("videoState", JSON.stringify(videoState));
 	}
 });
 
 window.addEventListener('pagereveal', async (e) => {
 	if (e.viewTransition) {
-		if (sessionStorage.getItem("currentTime")) {
+		const $video = document.querySelector('video');
+		const videoState = sessionStorage.getItem("videoState");
+		if ($video && videoState) {
 			try {
-				document.querySelector('video').currentTime = sessionStorage.getItem("currentTime");
-				document.querySelector('video').play();
-			} catch {
-				document.querySelector('video').currentTime = 0;
+				$video.addEventListener("loadeddata", e => {
+					if ($video.readyState >= 2) {
+						restoreVideoState($video, JSON.parse(videoState));
+					}
+				});
+			} catch (e) {
+				$video.currentTime = 0;
 			}
 		}
 	}
