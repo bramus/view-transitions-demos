417f87f19be50c6001b0cb10bf4d72fb46fdc040
Bramus <bramus@bram.us>
2024-05-19 05:27:30 +0200

    ✨ Pagination: Add SPA version that uses transitionHelper

--- src/pagination/shared/util.js
+++ src/pagination/shared/util.js
@@ -105,4 +105,38 @@ const generatePagination = (currentPageIndex, numPages, pathName = 'index.html')
 	return $ul;
 }
 
-export { clamp, createElement, generatePagination };
+function transitionHelper({
+	skipTransition = false,
+	types = [],
+	update,
+}) {
+
+	const unsupported = (error) => {
+		const updateCallbackDone = Promise.resolve(update()).then(() => {});
+
+		return {
+			ready: Promise.reject(Error(error)),
+			updateCallbackDone,
+			finished: updateCallbackDone,
+			skipTransition: () => {},
+			types,
+		};
+	}
+
+	if (skipTransition || !document.startViewTransition) {
+		return unsupported('View Transitions are not supported in this browser');
+	}
+
+	try {
+		const transition = document.startViewTransition({
+			update,
+			types,
+		});
+
+		return transition;
+	} catch (e) {
+		return unsupported('View Transitions with types are not supported in this browser');
+	}
+}
+
+export { clamp, createElement, generatePagination, transitionHelper };
---
+++ src/pagination/spa-types-with-helper/scripts.js
@@ -0,0 +1,45 @@
+import { clamp, createElement, generatePagination, transitionHelper } from './../shared/util.js';
+
+// Config
+const numPages = 4;
+let currentPageIndex = clamp(parseInt(new URL(window.location.href).searchParams.get('p') ?? 1), 1, numPages);
+
+// Write pagination on load
+document.querySelector('#app').appendChild(generatePagination(
+	currentPageIndex,
+	numPages,
+	new URL(window.location.href).pathname
+));
+
+// Handle Pagination clicks
+document.querySelector('#app').addEventListener('click', async e => {
+	if (e.target.tagName == 'A') {
+		// Extract Destination Page Index
+		const destinationPageIndex = parseInt(new URL(e.target.href).searchParams.get('p'));
+
+		// Determine direction
+		let direction = 'unknown';
+		if (currentPageIndex > destinationPageIndex) {
+			direction = 'backwards';
+		}
+		if (currentPageIndex < destinationPageIndex) {
+			direction = 'forwards';
+		}
+
+		// Prevent navigation
+		e.preventDefault();
+
+		// Inject new Pagination using a VT
+		transitionHelper({
+			update: () => {
+				document.querySelector('#app').innerText = '';
+				document.querySelector('#app').appendChild(generatePagination(
+					destinationPageIndex,
+					numPages,
+					new URL(window.location.href).pathname
+				));
+			},
+			types: [direction],
+		});
+	}
+});
